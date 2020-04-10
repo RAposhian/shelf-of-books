ef81fcf4463283a145548b8542c6f13452704d4d
Raposhian <ryan.aposhian@gmail.com>
2020-04-10 13:39:53 -0600

    finished auth endpoints and methods

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -20,9 +20,24 @@ module.exports = {
       res.status(201).send(req.session.user)
 
    },
-   login: (req, res) => {
+   login: async (req, res) => {
       const db = req.app.get('db');
       const {username, password} = req.body;
+
+      let checkUser = await db.auth.check_user(username);
+      if(!checkUser[0]) {
+         return res.status(400).send('Username or Password is incorrect')
+      }
+
+      const authorized = bcrypt.compareSync(password, checkUser[0].password);
+      if (!authorized) {
+         return res.status(400).send('Username or Password is incorrect')
+      }
+
+      delete checkUser[0].password
+
+      req.session.user = checkUser[0]
+      res.status(200).send(req.session.user)
    },
    logout: (req, res) => {
       req.session.destroy();
